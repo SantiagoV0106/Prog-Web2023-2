6500401f7cab8ecf46360f4943815d32ed64634c
SantiagoV0106 <velascosantiago3@gmail.com>
2023-09-28 12:20:22 -0500

    Listo Custom Hooks, falta error boton y CSS

--- cats-facts/src/App.jsx
+++ cats-facts/src/App.jsx
@@ -1,60 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Header, CatImg, Catfact, Button } from "./components";
-import { fetchFacts } from "./services/fetchCatFact";
-import { fetchImg } from "./services/fetchCatImg";
+import { useApp } from "./hooks/useApp";
 
 export function App() {
-
-    const [data, setData] = useState(null)
-    const [isLoading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-    const [img, setImg] = useState(null)
-    const [words, setWords] = useState(null)
-
-    const handleOnClick = () => {
-        fetchImgData()
-        fetchData()
-    }
-
-
-    const fetchData = () => {
-        setLoading(true)
-
-        fetchFacts()
-            .then((res) => {
-                setData(res)
-                if (res) {
-                    const wordsArray = res.split(' ').slice(0, 4);
-                    setWords(wordsArray.join(' '));
-                }
-            })
-            .catch((error) => setError(error))
-    }
-
-    useEffect(() => {
-        fetchData()
-        }, [])
-        
-    const fetchImgData = () => {
-        if (words) {
-            fetchImg({words})
-                .then((img) => {
-                    setImg(img)
-                })
-                .catch((error) => setError(error))
-                .finally(() => setLoading(false))
-        }
-    } 
-
-        useEffect(() => {
-            fetchImgData()
-    }, [words])
-
-    console.log(data);
-    console.log(words);
-    console.log(img);
-
   
+    const {data, isLoading, error, img, words, handleOnClick} = useApp()
+
+console.log(data);
+console.log(words);
+console.log(img);
+
     if (isLoading) {
         return (
             <>
@@ -64,7 +19,7 @@ export function App() {
         
         )
     }
-
+    
     if (error) {
 
         return (<p> {error} </p>)
---
+++ cats-facts/src/hooks/useApp.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import { fetchFacts } from "../services/fetchCatFact";
+import { fetchImg } from "../services/fetchCatImg";
+
+export const useApp = () => {
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const [img, setImg] = useState(null)
+    const [words, setWords] = useState(null)
+    
+    const handleOnClick = () => {
+        fetchImgData()
+        fetchData()
+    }
+    
+    
+    const fetchData = () => {
+        setLoading(true)
+    
+        fetchFacts()
+            .then((res) => {
+                setData(res)
+                if (res) {
+                    const wordsArray = res.split(' ').slice(0, 4);
+                    setWords(wordsArray.join(' '));
+                }
+            })
+            .catch((error) => setError(error))
+    }
+    
+    useEffect(() => {
+        fetchData()
+        }, [])
+        
+    const fetchImgData = () => {
+        if (words) {
+            fetchImg({words})
+                .then((img) => {
+                    setImg(img)
+                })
+                .catch((error) => setError(error))
+                .finally(() => setLoading(false))
+        }
+    } 
+    
+        useEffect(() => {
+            fetchImgData()
+    }, [words])
+ return {data, isLoading, error, img, words, handleOnClick}
+}
+
+
